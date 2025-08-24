3956a28253c6728efb12ca209961d16d142fb03f
Alexandre-Jasmin <146493685+Alexandre-Jasmin@users.noreply.github.com>
2025-08-24 17:07:43 -0400

    Initial Publish

---
+++ README.md
@@ -0,0 +1,2 @@
+# alex-teaches
+ Alex Teaches
---
+++ flask_app.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2024 Alexandre Jasmin All rights reserved.
+from flask import Flask, request, render_template, redirect, url_for, session, flash
+from flask_mail import Mail, Message
+import json
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+# Flask-Mail configuration
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'alexwinnable@gmail.com'
+app.config['MAIL_PASSWORD'] = 'qbqk bvmk yvur stfw'
+
+mail = Mail(app)
+
+@app.route('/')
+@app.route('/home')
+@app.route('/accueil')
+def index():
+    # Accueil
+    return render_template(
+        "index.html",
+    )
+
+@app.errorhandler(Exception)
+def handle_error(error):
+    # Get the error code, default to 500
+    error_code = getattr(error, 'code', 500)
+    return render_template('error.html', error_code=error_code), error_code
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
